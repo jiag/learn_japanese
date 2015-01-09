ead5dc9337425ba7a166e343649e01d13e4ba979
jiag <jiaaguu@gmail.com>
2015-01-08 18:51:30 -0800

    rewrite the cards generation script

--- python_kivy_method/spelling_cards.py
+++ python_kivy_method/spelling_cards.py
@@ -1,7 +1,12 @@
 # coding:utf-8
+#!/usr/bin/env python
 
-# This file is used to generate images cards
+# This file is used to generate different sounds or spelling cards
 from PIL import Image, ImageFont, ImageDraw
+from words_sounds import *
+import sys, string
+
+optionset = set(['hiragana', 'katakana', 'sounds'])
 
 def add_corners(im, rad):
     circle = Image.new('L', (rad * 2, rad * 2), 0)
@@ -16,70 +21,28 @@ def add_corners(im, rad):
     im.putalpha(alpha)
     return im
 
-# main_sound = []
+def generate_cards(card_type):
+    sounds = basic_sounds
 
-# extra_sound1 = []
+    if card_type == "hiragana":
+        words = basic_hiragana
+        font = ImageFont.truetype("./KaoriGel.ttf",50)
+    elif card_type == "katakana":
+        words = basic_katakana
+        font = ImageFont.truetype("./KaoriGel.ttf",50)
+    elif card_type == "sounds":
+        font = ImageFont.truetype("./SourceSansPro-Regular.otf",50)
+        words = sounds
 
-# extra_sound2 = []
+    for i in range(len(words)):
+        img = Image.new("RGBA",(200,200),(255,255,255,0))
+        img = add_corners(img, 40)
+        draw = ImageDraw.Draw(img)
+        draw.text((65,65), words[i],font=font,fill=(0,0,0))
+        img.save(sounds[i]+"_"+card_type+".png","PNG")
 
-# words = main_sound+extra_sound1+extra_sound2
-
-words = [u"あ",u"い",u"う",u"え",u"お",
-        u"か", u"き", u"く", u"け", u"こ",
-        u"さ", u"し", u"す", u"せ", u"そ",
-        u"た", u"ち", u"つ", u"て", u"と",
-        u"な", u"に", u"ぬ", u"ね", u"の",
-        u"は", u"ひ", u"ふ", u"へ", u"ほ",
-        u"ま", u"み", u"む", u"め", u"も",
-        u"や", u"ゆ", u"よ",
-        u"ら", u"り", u"る", u"れ", u"ろ",
-        u"わ", u"を", u"ん"]
-
-#extra_hiragana = [u"きゃ",　u"きゅ",　u"きょ",
-#                  u"しゃ",  u"しゅ",　u"しょ"]
-
-
-
-
-
-main_sound = ["a", "i", "u", "e", "o",
-              "ka", "ki", "ku", "ke", "ko",
-              "sa", "shi", "su", "se", "so",
-              "ta", "chi", "tsu", "te", "to",
-              "na", "ni", "nu", "ne", "no",
-              "ha", "hi", "fu", "he", "ho",
-              "ma", "mi", "mu", "me", "mo",
-              "ya","yu","yo",
-              "ra", "ri", "ru", "re", "ro",
-              "wa", "o", "n"]
-
-extra_sound1 = ["kya", "kyu", "kyo",
-                "sha", "shu", "sho",
-                "cha", "chu", "cho",
-                "nya", "nyu", "nyo",
-                "hya", "hyu", "hyo",
-                "mya", "myu", "myo",
-                "rya", "ryu", "ryo",
-                "gya", "gyu", "gyo",
-                "ja",  "ju",  "jo",
-                "bya", "byu", "byo",
-                "pya", "pyu", "pyo"]
-
-extra_sound2 = ["ga", "gi", "gu", "ge", "go",
-                "za", "ji", "zu", "ze", "zo",
-                "da", "ji", "zu", "de", "do",
-                "ba", "bi", "bu", "be", "bo",
-                "pa", "pi", "pu", "pe", "po"]
-
-
-sounds = main_sound#+extra_sound1+extra_sound2
-
-font = ImageFont.truetype("./KaoriGel.ttf",50)
-
-#draw.text((200,200), "Te")#,font=font)
-for i in range(len(words)):
-    img = Image.new("RGBA",(200,200),(255,255,255,0))
-    img = add_corners(img, 40)
-    draw = ImageDraw.Draw(img)
-    draw.text((65,65), words[i],font=font,fill=(0,0,0))
-    img.save(sounds[i]+"_Hiragana.png","PNG")
+if __name__ == "__main__":
+    par1 = sys.argv[1]
+    par1 = par1.lower()
+    if par1 in optionset:
+        generate_cards(par1)
---
+++ python_kivy_method/words_sounds.py
@@ -0,0 +1,74 @@
+# coding:utf-8
+
+basic_hiragana = [u"あ",u"い",u"う",u"え",u"お",
+                  u"か", u"き", u"く", u"け", u"こ",
+                  u"さ", u"し", u"す", u"せ", u"そ",
+                  u"た", u"ち", u"つ", u"て", u"と",
+                  u"な", u"に", u"ぬ", u"ね", u"の",
+                  u"は", u"ひ", u"ふ", u"へ", u"ほ",
+                  u"ま", u"み", u"む", u"め", u"も",
+                  u"や", u"ゆ", u"よ",
+                  u"ら", u"り", u"る", u"れ", u"ろ",
+                  u"わ", u"を", u"ん"]
+
+vocal_hiragana = [u"が", u"ぎ", u"ぐ", u"げ", u"ご", 
+                  u"ざ", u"じ", u"ず", u"ぜ", u"ぞ", 
+                  u"だ", u"じ", u"ず", u"で", u"ど", 
+                  u"ば", u"び", u"ぶ", u"べ", u"ぼ", 
+                  u"ぱ", u"ぴ", u"ぷ", u"ぺ", u"ぽ"]
+
+
+
+
+
+
+basic_katakana = [u"ア", u"イ", u"ウ", u"エ", u"オ", 
+                  u"カ", u"キ", u"ク", u"ケ", u"コ", 
+                  u"サ", u"シ", u"ス", u"セ", u"ソ", 
+                  u"タ", u"チ", u"ツ", u"テ", u"ト", 
+                  u"ナ", u"ニ", u"ヌ", u"ネ", u"ノ", 
+                  u"ハ", u"ヒ", u"フ", u"ヘ", u"ホ", 
+                  u"マ", u"ミ", u"ム", u"メ", u"モ", 
+                  u"ヤ", u"ユ", u"ヨ", 
+                  u"ラ", u"リ", u"ル", u"レ", u"ロ", 
+                  u"ワ", u"ヲ", u"ン"]
+
+vocal_katakana = [u"ガ", u"ギ", u"グ", u"ゲ", u"ゴ", 
+                  u"ザ", u"ジ", u"ズ", u"ゼ", u"ゾ", 
+                  u"ダ", u"じ", u"ず", u"デ", u"ド", 
+                  u"バ", u"ビ", u"ブ", u"ベ", u"ボ", 
+                  u"パ", u"ピ", u"プ", u"ペ", u"ポ"]
+
+
+
+
+basic_sounds = ["a", "i", "u", "e", "o",
+              "ka", "ki", "ku", "ke", "ko",
+              "sa", "shi", "su", "se", "so",
+              "ta", "chi", "tsu", "te", "to",
+              "na", "ni", "nu", "ne", "no",
+              "ha", "hi", "fu", "he", "ho",
+              "ma", "mi", "mu", "me", "mo",
+              "ya","yu","yo",
+              "ra", "ri", "ru", "re", "ro",
+              "wa", "o", "n"]
+
+comb_sounds = ["kya", "kyu", "kyo",
+                "sha", "shu", "sho",
+                "cha", "chu", "cho",
+                "nya", "nyu", "nyo",
+                "hya", "hyu", "hyo",
+                "mya", "myu", "myo",
+                "rya", "ryu", "ryo",
+                "gya", "gyu", "gyo",
+                "ja",  "ju",  "jo",
+                "bya", "byu", "byo",
+                "pya", "pyu", "pyo"]
+
+vocal_sounds = ["ga", "gi", "gu", "ge", "go",
+                "za", "ji", "zu", "ze", "zo",
+                "da", "ji", "zu", "de", "do",
+                "ba", "bi", "bu", "be", "bo",
+                "pa", "pi", "pu", "pe", "po"]
+
+
